6fda274390fb640994cd2a3498101332a8ab23ad
Philippe Normand <philn@igalia.com>
2023-06-05 09:13:18 +0100

    wip

--- Source/WebCore/Modules/speech/SpeechRecognizerGStreamer.cpp
+++ Source/WebCore/Modules/speech/SpeechRecognizerGStreamer.cpp
@@ -51,6 +51,57 @@ GST_DEBUG_CATEGORY(webkit_speech_recognizer_debug);
 
 namespace WebCore {
 
+static void high_pass_filter(Vector<float> & data, float cutoff, float sample_rate) {
+    const float rc = 1.0f / (2.0f * M_PI * cutoff);
+    const float dt = 1.0f / sample_rate;
+    const float alpha = dt / (rc + dt);
+
+    float y = data[0];
+
+    for (size_t i = 1; i < data.size(); i++) {
+        y = alpha * (y + data[i] - data[i - 1]);
+        data[i] = y;
+    }
+}
+
+static bool vad_simple(Vector<float> & pcmf32, int sample_rate, int last_ms, float vad_thold, float freq_thold, bool verbose) {
+    const int n_samples      = pcmf32.size();
+    const int n_samples_last = (sample_rate * last_ms) / 1000;
+
+    if (n_samples_last >= n_samples) {
+        // not enough samples - assume no speech
+        return false;
+    }
+
+    if (freq_thold > 0.0f) {
+        high_pass_filter(pcmf32, freq_thold, sample_rate);
+    }
+
+    float energy_all  = 0.0f;
+    float energy_last = 0.0f;
+
+    for (int i = 0; i < n_samples; i++) {
+        energy_all += fabsf(pcmf32[i]);
+
+        if (i >= n_samples - n_samples_last) {
+            energy_last += fabsf(pcmf32[i]);
+        }
+    }
+
+    energy_all  /= n_samples;
+    energy_last /= n_samples_last;
+
+    if (verbose) {
+        fprintf(stderr, "%s: energy_all: %f, energy_last: %f, vad_thold: %f, freq_thold: %f\n", __func__, energy_all, energy_last, vad_thold, freq_thold);
+    }
+
+    if (energy_last > vad_thold*energy_all) {
+        return false;
+    }
+
+    return true;
+}
+
 void SpeechRecognizer::dataCaptured(const MediaTime& time, const PlatformAudioData& audioData, const AudioStreamDescription& description, size_t sampleCount)
 {
     const auto& data = static_cast<const GStreamerAudioData&>(audioData);
@@ -78,15 +129,42 @@ void SpeechRecognizer::dataCaptured(const MediaTime& time, const PlatformAudioDa
         }
 
         float* s = (float*) out;
-        for (unsigned i = 0; i < outSize / GST_AUDIO_INFO_BPF(&m_outputStreamDescription); i++)
-            m_buffer.append((float)s[i]);
+        // for (unsigned i = 0; i < outSize / GST_AUDIO_INFO_BPF(&m_outputStreamDescription); i++)
+        //     m_buffer.append((float)s[i]);
+        size_t i = 0;
+        while (i < sampleCount) {
+            m_buffer.append((float)s[i++]);
+        }
         m_queuedSampleCount += sampleCount;
         g_free(out);
+
+        size_t n_samples = (WHISPER_SAMPLE_RATE * 2000) / 1000;
+
+        if (m_buffer.size() < n_samples)
+            return;
+
+        Vector<float> pcmf32_new;
+        pcmf32_new.resize(n_samples);
+        size_t j = 0;
+
+        for (auto it = m_buffer.begin(); it != m_buffer.end(); ++it) {
+            if (std::distance(m_buffer.begin(), it) < n_samples)
+                continue;
+            pcmf32_new[j++] = *it;
+        }
+        while (j < n_samples) {
+            pcmf32_new[j++] = 0.0;
+        }
+
+        if (!vad_simple(pcmf32_new, WHISPER_SAMPLE_RATE, 1000, 0.6, 100.0, false)) {
+            return;
+        }
     }
 
-    MediaTime queuedTime((m_queuedSampleCount * G_USEC_PER_SEC) / WHISPER_SAMPLE_RATE, G_USEC_PER_SEC);
-    if (queuedTime.toFloat() < 10)
-        return;
+    // MediaTime queuedTime((m_queuedSampleCount * G_USEC_PER_SEC) / WHISPER_SAMPLE_RATE, G_USEC_PER_SEC);
+    // if (queuedTime.toFloat() < 10)
+    //     return;
+
 
     // High pass filter
     // {
