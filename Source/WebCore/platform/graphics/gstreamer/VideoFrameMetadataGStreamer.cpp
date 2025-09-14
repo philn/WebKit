07ecad1cfa02f6a016909e00c8bea4909d313ec1
Philippe Normand <philn@igalia.com>
2025-09-14 16:37:53 +0100

    fixup! [GStreamer][MediaStream] Fix various leaks reported by the GStreamer leak tracer https://bugs.webkit.org/show_bug.cgi?id=298834

--- Source/WebCore/platform/graphics/gstreamer/VideoFrameMetadataGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/VideoFrameMetadataGStreamer.cpp
@@ -64,16 +64,16 @@ GType videoFrameMetadataAPIGetType()
 
 const GstMetaInfo* videoFrameMetadataGetInfo();
 
-std::pair<GstBuffer*, VideoFrameMetadataGStreamer*> ensureVideoFrameMetadata(GstBuffer* buffer)
+static std::pair<GRefPtr<GstBuffer>, VideoFrameMetadataGStreamer*> ensureVideoFrameMetadata(GRefPtr<GstBuffer>&& buffer)
 {
-    auto* meta = getInternalVideoFrameMetadata(buffer);
+    auto* meta = getInternalVideoFrameMetadata(buffer.get());
     if (meta)
-        return { buffer, meta };
+        return { GRefPtr(buffer), meta };
 
-IGNORE_WARNINGS_BEGIN("cast-align")
-    buffer = gst_buffer_make_writable(buffer);
-IGNORE_WARNINGS_END
-    return { buffer, VIDEO_FRAME_METADATA_CAST(gst_buffer_add_meta(buffer, videoFrameMetadataGetInfo(), nullptr)) };
+    IGNORE_WARNINGS_BEGIN("cast-align");
+    auto modifiedBuffer = adoptGRef(gst_buffer_make_writable(buffer.leakRef()));
+    IGNORE_WARNINGS_END;
+    return { modifiedBuffer, VIDEO_FRAME_METADATA_CAST(gst_buffer_add_meta(modifiedBuffer.get(), videoFrameMetadataGetInfo(), nullptr)) };
 }
 
 const GstMetaInfo* videoFrameMetadataGetInfo()
@@ -96,7 +96,7 @@ const GstMetaInfo* videoFrameMetadataGetInfo()
                     return FALSE;
 
                 auto* frameMeta = VIDEO_FRAME_METADATA_CAST(meta);
-                auto [buf, copyMeta] = ensureVideoFrameMetadata(buffer);
+                auto [buf, copyMeta] = ensureVideoFrameMetadata(GRefPtr(buffer));
                 copyMeta->priv->videoSampleMetadata = frameMeta->priv->videoSampleMetadata;
                 copyMeta->priv->rotation = frameMeta->priv->rotation;
                 copyMeta->priv->isMirrored = frameMeta->priv->isMirrored;
@@ -167,8 +167,8 @@ void webkitGstTraceProcessingTimeForElement(GstElement* element)
     static auto probeType = static_cast<GstPadProbeType>(GST_PAD_PROBE_TYPE_PUSH | GST_PAD_PROBE_TYPE_BUFFER);
 
     gst_pad_add_probe(sinkPad.get(), probeType, [](GstPad*, GstPadProbeInfo* info, gpointer userData) -> GstPadProbeReturn {
-        auto [modifiedBuffer, meta] = ensureVideoFrameMetadata(GST_PAD_PROBE_INFO_BUFFER(info));
-        gst_pad_probe_info_set_buffer(info, gst_buffer_ref(modifiedBuffer));
+        auto [modifiedBuffer, meta] = ensureVideoFrameMetadata(GRefPtr(GST_PAD_PROBE_INFO_BUFFER(info)));
+        gst_pad_probe_info_set_buffer(info, modifiedBuffer.leakRef());
         Locker locker { meta->priv->lock };
         meta->priv->processingTimes.set(GST_ELEMENT_CAST(userData), std::make_pair(gst_util_get_timestamp(), GST_CLOCK_TIME_NONE));
         return GST_PAD_PROBE_OK;
